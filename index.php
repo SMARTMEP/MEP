f7a5eec313c733b5459f1292045d9c623f50067b
SMARTMEP <124365834+SMARTMEP@users.noreply.github.com>
2023-02-10 17:46:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,12 @@ $name = $_POST['Name'];
 $email= $_POST['Email'];
 $subject= $_POST['Subject'];
 $comment= $_POST['Comment'];
-
 $to = "smartmeptraining@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n subject =" . $subject;\r\n comment =" . $comment;
 $headers = "https://smartmep.github.io/MEP/" . "\r\n" .
 "CC: nayakrakesh868@gmail.com";
-if($email!=NULL){
+if($Email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
