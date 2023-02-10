114e1333c1e848f6ef14e68492f26bfec87e46ea
SMARTMEP <124365834+SMARTMEP@users.noreply.github.com>
2023-02-10 17:37:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+//get data from form  
+$name = $_POST['Name'];
+$email= $_POST['Email'];
+$subject= $_POST['Subject'];
+$comment= $_POST['Comment'];
+
+$to = "smartmeptraining@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n subject =" . $subject;\r\n comment =" . $comment;
+$headers = "https://smartmep.github.io/MEP/" . "\r\n" .
+"CC: nayakrakesh868@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index1.html");
+?>
